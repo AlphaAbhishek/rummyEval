f833a7d37520b29bf3c25e14fd007ef0518d5d14
Shivaram <sshivaram@paypalcorp.com>
2016-08-11 11:29:05 +0530

    Added return sorted string function

--- src/rummyEval/core/EvaluatorUtility.java
+++ src/rummyEval/core/EvaluatorUtility.java
@@ -1,10 +1,13 @@
 package rummyEval.core;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import rummyEval.entity.Card;
 
 public class EvaluatorUtility {
+	List<Card> cards = new ArrayList<Card>();
 
 	private static String sequences = "23456789TJQKA";
 	
@@ -22,10 +25,39 @@ public class EvaluatorUtility {
 		return false;
 	}
 	
+	@SuppressWarnings("static-access")
 	public String getModifiedString(RummyHand r,Card joker)
 	{
-		//should give a string of sorted cards according to Suit. Separate Suits by space. Represent jokers by x.
+		//should give a string of sorted cards according to Suit. Separate Suits by space. 
+		//Represent jokers by x.
 		//Ex - C2 D4 D5 H7 JOKER H8 CJ => "X 2J 45 78"
-		return null;
+		List<Card> cardList = new ArrayList<Card>();
+		List<Card> sortedByValueCardList = new ArrayList<Card>();
+		List<Card> sortedBySuitCardList = new ArrayList<Card>();
+		String outputString = "";
+		cardList = r.getRummyHandCards();
+		for(Card card: cards) {
+			if(card.getFaceValues().equals("Joker")||card.getFaceValues().compareTo(joker.getFaceValues())==0) {
+				outputString+="x"+"\t";
+				cardList.remove(card);
+			}
+		}
+		sortedByValueCardList = sortByValue(cardList);
+		sortedBySuitCardList = sortBySuit(sortedByValueCardList);
+		for(Card card: sortedBySuitCardList) {
+			outputString+=card.toString()+"\t";
+		}
+		return outputString;
+	}
+
+	private List<Card> sortBySuit(List<Card> sortedByValueCardList) {
+		Collections.sort(sortedByValueCardList,Card.suitComparator);
+		return sortedByValueCardList;
+	}
+
+	private List<Card> sortByValue(List<Card> cardList) {
+		// TODO Auto-generated method stub
+		Collections.sort(cardList,Card.valueComparator);
+		return cardList;
 	}
 }
